df5dd7798e55dfeb7acb00568c316ebd6036c620
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2022-01-23 18:39:38 -0500

    ensayo21

--- preguntas.py
+++ preguntas.py
@@ -188,7 +188,7 @@ def pregunta_10():
 
     tbl0_copyOrdeFil = tbl0_copyOrde[['_c1', 'New']]
 
-    tbl0_copyOrdeFil1 = tbl0_copyOrdeFil.rename(columns={'_c1':'_c0', 'New':'_c1'})
+    tbl0_copyOrdeFil1 = tbl0_copyOrdeFil.rename(columns={'_c1':'_c1', 'New':'_c2'})
     tbl0_copyOrdeFil2 = tbl0_copyOrdeFil1.drop_duplicates()
 
     rest1 = tbl0_copyOrdeFil2.sort_values(by=['_c0'])
